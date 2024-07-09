df515ce75362fa6fc82ba4f99d0a9617aa818ad1
Yohanesk <y.koeswono2311@gmail.com>
2024-07-09 14:13:42 +0700

    Add base HTML template with Bootstrap and Font Awesome integration

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <!-- 
+        The <head> section contains metadata about the HTML document,
+        including the character set, viewport settings, title, and 
+        links to external CSS and JS resources such as Bootstrap and 
+        Font Awesome. 
+    -->
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book List</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <!-- 
+        The <body> section contains the content of the HTML document.
+        It includes a main container for the content and scripts for
+        Bootstrap, Popper.js, and Font Awesome. It also has a section 
+        for custom JavaScript.
+    -->
+    <div class="container mt-5">
+        @yield('content')
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,25 +1,34 @@
-
+@extends('components.header') 
+<!-- Extending the header component -->
+@section('content') 
+<!-- Starting the content section -->
     <div class="row">
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Add a Book</h5>
+                    <!-- Form for adding a book -->
                     <form id="bookForm" method="post">
-                        @csrf
+                        @csrf 
+                        <!-- CSRF token for security -->
                         <div class="form-group">
                             <label for="title">Title</label>
                             <input type="text" class="form-control" name="title" id="title"
                                    placeholder="Enter book title" required>
+                            <!-- Input for book title -->
                         </div>
                         <div class="form-group">
                             <label for="author">Author</label>
                             <input type="text" class="form-control" name="author" id="author"
-                                   placeholder="Enter author name"
-                                   required>
+                                   placeholder="Enter author name" required>
+                            <!-- Input for author name -->
                         </div>
                         <button type="submit" class="btn btn-primary mt-3">Add</button>
+                        <!-- Submit button -->
                     </form>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+<!-- Ending the content section -->
