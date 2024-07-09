4bd99873f84bef2dd10b31aaf3f662317beeabee
Yohanesk <y.koeswono2311@gmail.com>
2024-07-09 21:06:40 +0700

    Add a section for CSV and XML Export in the display

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -30,7 +30,9 @@
                 </div>
             </div>
         </div>
+
         <div class="col-lg-12 mt-5">
+            <div class="row">
                 <div class="col-md-6 col-lg-6">
                     <div class="card">
                         <div class="card-body">
@@ -39,8 +41,7 @@
                                 <div class="form-group">
                                     <label for="author">Search by Title / Author</label>
                                     <input value="{{request()->search}}" type="text" class="form-control" name="search"
-                                           placeholder="Search .."
-                                           required>
+                                           placeholder="Search .." required>
                                 </div>
                                 <button type="submit" class="btn btn-primary mt-3">Search</button>
                                 <a href="{{route('home')}}" class="btn btn-secondary mt-3">Clear</a>
@@ -48,8 +49,48 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-md-6 col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Export</h5>
+                            <label>Choose Fields and Format</label>
+                            <form method="post">
+                                @csrf
+                                <div class="d-flex justify-content-between">
+                                    <div class="d-flex">
+                                        <div class="form-check ml-5" style="margin: 5px 40px 5px 5px">
+                                            <input class="form-check-input" name="fields[]" type="checkbox" value="author"
+                                                   id="flexCheckDefault1">
+                                            <label class="form-check-label" for="flexCheckDefault1">
+                                                Author
+                                            </label>
+                                        </div>
+                                        <div class="form-check ml-5" style="margin: 5px">
+                                            <input class="form-check-input" name="fields[]" type="checkbox" value="title"
+                                                   id="flexCheckChecked">
+                                            <label class="form-check-label" for="flexCheckChecked">
+                                                Title
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                                            <input type="radio" class="btn-check" name="export_type" id="btnradio1" value="csv" checked>
+                                            <label class="btn btn-outline-primary waves-effect" for="btnradio1">CSV</label>
+
+                                            <input type="radio" class="btn-check" name="export_type" id="btnradio2" value="xml">
+                                            <label class="btn btn-outline-primary waves-effect" for="btnradio2">XML</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary mt-3">Export</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+        
         <div class="col-lg-12 mt-5">
             <div class="card">
                 <div class="card-body">
@@ -57,30 +98,30 @@
                     <div class="table-responsive">
                         <table class="table table-striped mt-4">
                             <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Author</th>
-                                <th>Delete</th>
-                            </tr>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Author</th>
+                                    <th>Delete</th>
+                                </tr>
                             </thead>
                             <tbody id="bookList">
-                            @foreach($books as $book)
-                                <tr>
-                                    <td>{{$book->title}}</td>
-                                    <td>{{$book->author}}</td>
-                                    <td>
-                                        <a href="{{route('delete_book',['id' => $book->id])}}"
-                                            onclick="return confirm('Do you want to delete this?')"
-                                            class="btn btn-danger btn-sm delete" data-id="{{$book->id}}"><i
-                                            class="fa fa-trash"></i>
-                                        </a>
-                                        <a href="{{route('edit_book',['id' => $book->id])}}"
-                                            class="btn btn-warning btn-sm edit" data-id="{{$book->id}}"><i
-                                            class="fa fa-pencil"></i>
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endforeach
+                                @foreach($books as $book)
+                                    <tr>
+                                        <td>{{$book->title}}</td>
+                                        <td>{{$book->author}}</td>
+                                        <td>
+                                            <a href="{{route('delete_book',['id' => $book->id])}}"
+                                               onclick="return confirm('Do you want to delete this?')"
+                                               class="btn btn-danger btn-sm delete" data-id="{{$book->id}}">
+                                                <i class="fa fa-trash"></i>
+                                            </a>
+                                            <a href="{{route('edit_book',['id' => $book->id])}}"
+                                               class="btn btn-warning btn-sm edit" data-id="{{$book->id}}">
+                                                <i class="fa fa-pencil"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
