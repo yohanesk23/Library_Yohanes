80288742329f4508c785b9d0d1a6dd9fd56568a1
Yohanesk <y.koeswono2311@gmail.com>
2024-07-09 15:52:32 +0700

    fix database connection and migration

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,6 +29,26 @@
                 </div>
             </div>
         </div>
+        <div class="col-lg-12 mt-5">
+                <div class="col-md-6 col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Search</h5>
+                            <form method="get">
+                                <div class="form-group">
+                                    <label for="author">Search by Title / Author</label>
+                                    <input value="{{request()->search}}" type="text" class="form-control" name="search"
+                                           placeholder="Search .."
+                                           required>
+                                </div>
+                                <button type="submit" class="btn btn-primary mt-3">Search</button>
+                                <a class="btn btn-secondary mt-3">Clear</a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 <!-- Ending the content section -->
